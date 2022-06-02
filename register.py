ab8aac99c645cc4b9f32c4d8b037b4824fa370d8
Subhadip De <88263132+subhadipde@users.noreply.github.com>
2022-06-03 03:51:35 +0530

    Update register.py

--- register.py
+++ register.py
@@ -16,11 +16,11 @@ class Register:
         # code for full screen height and width --------------------
         width= root_login.winfo_screenwidth()               
         height= root_login.winfo_screenheight()               
-        # root_login.geometry("%dx%d" % (width, height))
+        root_login.geometry("%dx%d" % (width, height))
         # for maximize the window ---------------------------
         root_login.state('zoomed') 
 
-        self.root_login.title("Login")
+        self.root_login.title("Register")
 
         # variables ----------
         self.var_fname = StringVar()
@@ -47,88 +47,97 @@ class Register:
         # left_lbl.place(x=50,y=100,width=470,height=550)
 
         # main frame
-        frame=Frame(self.root_login,bg="black")
-        frame.place(relx=0.5, rely=0.5, anchor=CENTER,width=800,height=550)
+        frame=ctk.CTkFrame(self.root_login,fg_color="black",corner_radius=10)
+        frame.place(relx=0.5, rely=0.5, anchor=CENTER,width=800,height=750)
 
-        register_lbl=Label(frame,text="REGISTER HERE",font=("times new roman",20,"bold"),fg="cyan",bg="black")
+        register_lbl=ctk.CTkLabel(frame,text="REGISTER HERE")
+        register_lbl.configure(font=("times new roman",25,"bold"),fg="white")
         register_lbl.place(relx=0.5, rely=0.1, anchor=CENTER,)
 
         # =============label and entry===============================
         #row1
-        fname = Label(frame,text="First Name", font=("times new roman", 15, "bold"), fg="white",bg="black")
-        fname.place(relx=0.15, rely=0.2, anchor=CENTER)
+        fname = ctk.CTkLabel(frame,text="First Name")
+        fname.configure(font=("times new roman",15,"bold"),fg="white")
+        fname.place(relx=0.16, rely=0.2, anchor=CENTER)
 
-        fname_entry = ttk.Entry(frame,textvariable=self.var_fname,font=("times new roman",15,"bold"))
-        fname_entry.place(relx=0.25, rely=0.25, anchor=CENTER, width=250)
+        fname_entry = ctk.CTkEntry(frame,textvariable=self.var_fname, width=200)
+        fname_entry.place(relx=0.25, rely=0.25, anchor=CENTER)
 
-        l_name = Label(frame,text="Last Name",font=("times new roman",15,"bold"),fg="white",bg="black")
-        l_name.place(relx=0.7, rely=0.2, anchor=CENTER)
+        l_name = ctk.CTkLabel(frame,text="Last Name")
+        l_name.configure(font=("times new roman",15,"bold"),fg="white")
+        l_name.place(relx=0.72, rely=0.2, anchor=CENTER)
 
-        self.txt_lname = ttk.Entry(frame,textvariable=self.var_lname,font=("times new roman",15)) #yaha confusion h lname me check kr lena
-        self.txt_lname.place(relx=0.8, rely=0.25, anchor=CENTER, width=250)
+        self.txt_lname = ctk.CTkEntry(frame,textvariable=self.var_lname, width=200)      #yaha confusion h lname me check kr lena
+        self.txt_lname.place(relx=0.8, rely=0.25, anchor=CENTER)
 
 
         #row2
-        contact = Label(frame,text="Contact No",font=("times new roman", 15, "bold"), fg="white",bg="black")
-        contact.place(relx=0.15, rely=0.35, anchor=CENTER)
+        contact = ctk.CTkLabel(frame,text="Contact No")
+        contact.configure(font=("times new roman",15,"bold"),fg="white")
+        contact.place(relx=0.16, rely=0.35, anchor=CENTER)
 
-        self.txt_contact = ttk.Entry(frame,textvariable=self.var_contact,font=("times new roman",15))
-        self.txt_contact.place(relx=0.25, rely=0.4, anchor=CENTER, width=250)
+        self.txt_contact = ctk.CTkEntry(frame,textvariable=self.var_contact, width=200)
+        self.txt_contact.place(relx=0.25, rely=0.4, anchor=CENTER)
 
-        email= Label(frame,text="Username",font=("times new roman",15,"bold"),fg="white",bg="black")
+        email= ctk.CTkLabel(frame,text="Email")
+        email.configure(font=("times new roman",15,"bold"),fg="white")
         email.place(relx=0.68, rely=0.35, anchor=CENTER)
 
-        self.txt_email = ttk.Entry(frame,textvariable=self.var_email,font=("times new roman",15))  
-        self.txt_email.place(relx=0.8, rely=0.4, anchor=CENTER, width=250)
+        self.txt_email = ctk.CTkEntry(frame,textvariable=self.var_email, width=200)  
+        self.txt_email.place(relx=0.8, rely=0.4, anchor=CENTER)
 
 
         #row3
-        security_Q = Label(frame, text="Select Security Question", font=("times new roman", 15, "bold"), fg="white",bg="black")
-        security_Q.place(relx=0.23, rely=0.5, anchor=CENTER)
+        security_Q = ctk.CTkLabel(frame, text="Select Security Question")
+        security_Q.configure(font=("times new roman",15,"bold"),fg="white")
+        security_Q.place(relx=0.25, rely=0.5, anchor=CENTER)
 
         self.combo_security_Q = ttk.Combobox(frame,textvariable=self.var_securityQ,font=("times new roman", 15, "bold"), state="readonly")
         self.combo_security_Q["values"] = ("Select", "Your Birth place", "Your Dad's name", "Your Mom's name")
         self.combo_security_Q.place(relx=0.25, rely=0.55, anchor=CENTER, width=250)
         self.combo_security_Q.current(0)
 
-        security_A = Label(frame, text="Security Answer", font=("times new roman", 15, "bold"), fg="white",bg="black")
-        security_A.place(relx=0.73, rely=0.5, anchor=CENTER)
+        security_A = ctk.CTkLabel(frame, text="Security Answer")
+        security_A.configure(font=("times new roman",15,"bold"),fg="white")
+        security_A.place(relx=0.75, rely=0.5, anchor=CENTER)
 
-        self.txt_security = ttk.Entry(frame,textvariable=self.var_securityA,font=("times new roman", 15))
-        self.txt_security.place(relx=0.8, rely=0.55, anchor=CENTER, width=250)
+        self.txt_security = ctk.CTkEntry(frame,textvariable=self.var_securityA, width=200)
+        self.txt_security.place(relx=0.8, rely=0.55, anchor=CENTER)
 
 
         #row4
-        pswd=Label(frame,text="Password",font=("times new roman",15,"bold"),fg="white",bg="black")
+        pswd=ctk.CTkLabel(frame,text="Password")
+        pswd.configure(font=("times new roman",15,"bold"),fg="white")
         pswd.place(relx=0.15, rely=0.65, anchor=CENTER)
 
-        self.txt_pswd= ttk.Entry(frame,textvariable=self.var_pass,font=("times new roman", 15))
-        self.txt_pswd.place(relx=0.25, rely=0.7, anchor=CENTER, width=250)
+        self.txt_pswd= ctk.CTkEntry(frame,textvariable=self.var_pass, width=200)
+        self.txt_pswd.place(relx=0.25, rely=0.7, anchor=CENTER)
 
-        confirm_pswd=Label(frame,text="Confirm Password",font=("times new roman",15,"bold"),fg="white",bg="black")
-        confirm_pswd.place(relx=0.74, rely=0.65, anchor=CENTER)
+        confirm_pswd=ctk.CTkLabel(frame,text="Confirm Password")
+        confirm_pswd.configure(font=("times new roman",15,"bold"),fg="white")
+        confirm_pswd.place(relx=0.77, rely=0.65, anchor=CENTER)
 
-        self.txt_confirm_pswd = ttk.Entry(frame,textvariable=self.var_confpass,font=("times new roman", 15))
-        self.txt_confirm_pswd.place(relx=0.8, rely=0.7, anchor=CENTER, width=250)
+        self.txt_confirm_pswd = ctk.CTkEntry(frame,textvariable=self.var_confpass, width=200)
+        self.txt_confirm_pswd.place(relx=0.8, rely=0.7, anchor=CENTER)
 
         # ......check button
         self.var_check = IntVar()
-        checkbtn = Checkbutton(frame,variable=self.var_check,text="I am Agreed with the terms and conditions", font=("times new roman", 8, "bold"), bg="white", onvalue=1, offvalue=0)
-        checkbtn.place(relx=0.5, rely=0.76,anchor=CENTER)
+        checkbtn = Checkbutton(frame,variable=self.var_check,text="I am Agreed with the terms and conditions", font=("times new roman", 12, "bold"), onvalue=1, offvalue=0)
+        checkbtn.place(relx=0.5, rely=0.8, anchor=CENTER)
 
         #buttons
         img = Image.open(r"images\register.jpg")
         img = img.resize((200, 55), Image.Resampling.LANCZOS)
         self.photoimage = ImageTk.PhotoImage(img)
         b1 = Button(frame,command=self.register_data,image=self.photoimage, borderwidth=0, cursor="hand2",font=("times new roman", 15, "bold"), bg="black")
-        b1.place(relx=0.3, rely=0.85, anchor=CENTER)
+        b1.place(relx=0.3, rely=0.9, anchor=CENTER)
 
 
         img1 = Image.open(r"images\login_now.jpg")
-        img1 = img1.resize((200, 45), Image.Resampling.LANCZOS)
+        img1 = img1.resize((200, 55), Image.Resampling.LANCZOS)
         self.photoimage1 = ImageTk.PhotoImage(img1)
         b1 = Button(frame,image=self.photoimage1, command=self.return_login, borderwidth=0, cursor="hand2",font=("times new roman", 15, "bold"), bg="black")
-        b1.place(relx=0.7, rely=0.85, anchor=CENTER)
+        b1.place(relx=0.7, rely=0.9, anchor=CENTER)
 
 
     # ...............function...................
@@ -146,7 +155,7 @@ class Register:
         elif self.var_check.get() == 0:
             messagebox.showerror("Error","Please agree our terms and condition",parent=self.root_login)
         else:
-            conn=mysql.connector.connect(host="localhost", username="root", password="Subhadip@321#", database="face_recognition")
+            conn=mysql.connector.connect(host="localhost", username="root", password="t00r", database="face_recognition")
             my_cursor=conn.cursor()
             query = ("select * from register where email=%s")
             value = (self.var_email.get(),)
